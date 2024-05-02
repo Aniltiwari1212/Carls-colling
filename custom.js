a2162b9d05a17f6599c3929ede2c856480ad09c2
Aniltiwari1212 <121211457+Aniltiwari1212@users.noreply.github.com>
2024-05-02 16:43:58 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,98 @@
+
+jQuery(document).ready(function(){ 
+ jQuery("#service-sec").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ });
+ 
+ jQuery("#service-sec-1").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ });
+ 
+ jQuery("#service-sec-2").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ });
+ 
+ jQuery("#service-sec-3").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ });
+ 
+ jQuery("#service-sec-4").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ });
+ 
+ jQuery("#service-sec-5").owlCarousel({
+  items:4,
+  loop:true,
+  autoplay:true,
+  margin:15,
+  nav:true,
+  autoplaySpeed:1000,
+  responsive:{
+  0:{ items:1 },
+  320:{ items:2 },
+  768:{ items:3 },
+  1200:{ items:4 }
+  }
+ }); 
+});
+
+$(document).ready(function() {
+ $('#lightgallery').lightGallery({
+   pager: true
+ });
+});
